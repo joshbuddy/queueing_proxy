9772e77f3daf9461df32847f0dd863dbca7a0b74
Brad Gessler <brad@bradgessler.com>
2011-09-13 01:19:58 -0700

    Fixed the client timeout. Fixed 500 statuses from crashing

--- lib/queueing_proxy/dispatcher.rb
+++ lib/queueing_proxy/dispatcher.rb
@@ -7,21 +7,21 @@ module QueueingProxy
     def initialize(logger, to_host, to_port, beanstalk_host, tube)
       @logger, @to_host, @to_port, @beanstalk_host, @tube = logger, to_host, to_port, beanstalk_host, tube
       @beanstalk = EMJack::Connection.new(:host => @beanstalk_host, :tube => @tube)
-      beanstalk.watch(@tube) # Connect to the tube and listen
+      # beanstalk.watch(@tube) # Connect to the tube and listen
     end
 
     def run
       logger.info "Worker #{object_id} at your service"
       beanstalk.reserve do |job|
         logger.info "Reserved #{job}"
-        upstream = Upstream.new(job.body, @to_host, @to_port, 30, logger).request
+        upstream = Upstream.new(job.body, @to_host, @to_port, 15, logger).request
         upstream.errback{
           logger.info "Ruh roh! Errback. Try again in 5 seconds. #{job}"
           job.release(:delay => 5)
           run # Call again
         }
         upstream.callback {
-          case Integer(upstream.response.status_code)
+          case status = Integer(upstream.response.status_code)
           when 200
             logger.info "Done dispatching #{job.jobid}"
             job.delete
@@ -41,9 +41,11 @@ module QueueingProxy
     class Upstream
       include EventMachine::Deferrable
 
+      # The connection handler for EM.
       module Client
         attr_accessor :client
 
+        # Succesful connection!
         def connection_completed
           # Setup our callback for when the upstream response hits us
           client.response.on_headers_complete = Proc.new {
@@ -56,41 +58,39 @@ module QueueingProxy
           send_data client.payload
         end
 
+        # Read the response of the upstream proxy
         def receive_data(data)
           # Send the upstream response into the HTTP parser
           client.response << data
         end
 
-        # Connection failures result in an unbinding
+        # If something bad happens to the connection, bind gets called
         def unbind
           client.fail
         end
       end
 
-      attr_accessor :payload, :host, :port, :ttl, :logger
+      attr_accessor :payload, :host, :port, :timeout, :logger
       attr_reader :response
 
-      def initialize(payload, host, port, ttl, logger)
+      def initialize(payload, host, port, timeout, logger)
         @response = Http::Parser.new
-        @payload, @host, @port, @ttl, @logger = payload, host, port, ttl, logger
+        @payload, @host, @port, @timeout, @logger = payload, host, port, timeout, logger
       end
 
+      # Connect to the upstream server and send the HTTP payload
       def request
         begin
-          EventMachine.connect(host, port, Client) {|c|
+          @connection = EventMachine.connect(host, port, Client) {|c|
             c.client = self
-            c.comm_inactivity_timeout = ttl
-            c.pending_connect_timeout = 2
+            c.comm_inactivity_timeout = timeout
+            c.pending_connect_timeout = timeout
           }
         rescue => e
           logger.error e
           fail # If something explodes here, fail the defferable
         end
         self
-      end
-
-      def start_ttl
-        EM::Timer.new(ttl){ fail } if ttl
       end
     end
   end
